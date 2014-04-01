a381487595c6a2a6fbb756880b588fa2aaad8817
Matheus Guilarducci <mguilarducci@gmail.com>
2014-04-01 15:21:03 -0300

    routes

--- src/helloclojure/core.clj
+++ src/helloclojure/core.clj
@@ -1,8 +1,16 @@
 (ns helloclojure.core
+	(:use compojure.core)
 	(:require
-	  [ring.adapter.jetty :as jetty]))
+	  [ring.adapter.jetty :as jetty]
+	  [compojure.route :as route]))
+
+(defroutes main-routes
+	(GET "/" [] 
+		(fn [req] {:status 200
+           :body "{\"text\":\"Hello World\"}"
+           :headers {"Content-Type" "application/json"}})))
 
 (defn -main []
-	(jetty/run-jetty
-		(fn [req] {:status 200 :body "hello world!"})
+	(jetty/run-jetty 
+		main-routes
 		{:port 5000}))
