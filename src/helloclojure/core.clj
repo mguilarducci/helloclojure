405005d96366624894542132d8e40fe431d01cfe
Matheus Guilarducci <mguilarducci@gmail.com>
2014-04-01 15:36:57 -0300

    templates

--- src/helloclojure/core.clj
+++ src/helloclojure/core.clj
@@ -2,14 +2,33 @@
 	(:use compojure.core)
 	(:require
 	  [ring.adapter.jetty :as jetty]
+	  [clostache.parser :as clostache]
 	  [compojure.route :as route]))
 
+(defn read-template [template-name]
+	(slurp (clojure.java.io/resource
+		(str "templates/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+	(clostache/render 
+		(read-template template-file) params))
+
+;views
+(defn index []
+	(render-template "index" {:greeting "OPA"}))
+
+;routes
 (defroutes main-routes
-	(GET "/" [] 
+	(GET "/json" [] 
 		(fn [req] {:status 200
            :body "{\"text\":\"Hello World\"}"
-           :headers {"Content-Type" "application/json"}})))
+           :headers {"Content-Type" "application/json"}}))
 
+	(GET "/" [] (index))
+	(route/resources "/")
+  	(route/not-found "404 Not Found"))
+
+;main
 (defn -main []
 	(jetty/run-jetty 
 		main-routes
